2963e54b8e41235eb918c937ffd42ee593337417
Dikshant Chitara <93652656+dikshantchitara@users.noreply.github.com>
2024-01-17 14:18:02 +0530

    Initial Links Added
    Shops Links Added to view and urls

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.Home,name="ShopHome"),
+    path('about/',views.aboutus,name="AboutUs"),
+    path('contact/',views.contact,name="ContactUs"),
+    path('tracker/',views.tracker,name="TrackingStatus"),
+    path('productview/',views.productview,name="Search"),
+    path('search/',views.search,name="Search"),
+    path('checkout/',views.checkout,name="checkout"),
+    
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def Home(request):
+    return render(request , 'shop/index.html')
+
+def aboutus(request):
+    return HttpResponse("we are at about us")
+
+ 
+def contact(request):
+    return HttpResponse("we are at contact us")
+
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+def productview(request):
+    return HttpResponse("we are at product view")
+
+def checkout(request):
+    return HttpResponse("we are at checkout page")
+
+def search(request):
+    return HttpResponse("we are at search field")
+ 
