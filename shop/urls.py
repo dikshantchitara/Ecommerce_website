a033720702782e7f12498826d21262555835fcc9
Dikshant Chitara <93652656+dikshantchitara@users.noreply.github.com>
2024-01-20 16:25:16 +0530

    Created a Model and Many Other Things added
    Contactus Model, About us and Click Here prodView

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('about/',views.aboutus,name="AboutUs"),
     path('contact/',views.contact,name="ContactUs"),
     path('tracker/',views.tracker,name="TrackingStatus"),
-    path('productview/',views.productview,name="Search"),
+    path('products/<int:myid>',views.productview,name="Search"),
     path('search/',views.search,name="Search"),
     path('checkout/',views.checkout,name="checkout"),
     
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models  import Product
+from .models  import Product,Contact
 from math import ceil
 
 def Home(request):
@@ -18,21 +18,30 @@ def Home(request):
     return render(request,"shop/index.html", params)
 
 def aboutus(request):
-    return HttpResponse("we are at about us")
+    return render(request,"shop/aboutus.html")
 
  
 def contact(request):
-    return HttpResponse("we are at contact us")
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, 'shop/Contactus.html')
+
 
 def tracker(request):
-    return HttpResponse("we are at tracker")
+    return render(request,"shop/tracker.html")
 
-def productview(request):
-    return HttpResponse("we are at product view")
+def productview(request,myid):
+    product = Product.objects.filter(id=myid)
+    return render(request,"shop/prodview.html" , {'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("we are at checkout page")
+    return render(request,"shop/checkout.html")
 
 def search(request):
-    return HttpResponse("we are at search field")
+    return render(request,"shop/search.html")
  
